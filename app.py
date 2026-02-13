12a9cd5162b17a2c356d2a5fdf1a7ed70d45ead8
thakurjaideep0123-ops <thakurjaideep0123@gmail.com>
2026-02-13 22:07:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("hi")
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/forgive")
+def forgive():
+    return jsonify({
+        "message": "Sona smiled… 💗 She forgives you. Forever."
+    })
+
+if __name__ == "__main__":
+    app.run()
